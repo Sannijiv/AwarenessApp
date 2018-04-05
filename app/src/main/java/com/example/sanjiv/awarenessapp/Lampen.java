bd3f1100d0c3ff20e979789be16b5f1e8b8a2d45
Sanjiv-PC\Sanjiv <Sanjivjankie@gmail.com>
2018-04-05 20:10:30 +0200

    Basic UI (Sidebar menu)

---
+++ app/src/main/java/com/example/sanjiv/awarenessapp/Lampen.java
@@ -0,0 +1,28 @@
+package com.example.sanjiv.awarenessapp;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Lampen extends Fragment {
+
+    public Lampen() {
+        // required empty constructor
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_lampen, null);
+        return v;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
---
+++ app/src/main/java/com/example/sanjiv/awarenessapp/Settings.java
@@ -0,0 +1,27 @@
+package com.example.sanjiv.awarenessapp;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Settings extends Fragment {
+
+    public Settings() {
+        // required empty constructor
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_instellingen, null);
+        return v;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
